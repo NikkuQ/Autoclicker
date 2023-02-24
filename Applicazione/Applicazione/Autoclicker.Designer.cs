ace3a5bf74ffec0b8fb4a13628da1dd1e71a9cc0
Nicola Casagrande <73555956+NikkuQ@users.noreply.github.com>
2023-02-24 15:41:27 +0100

    Add files via upload

---
+++ Applicazione/Applicazione/Autoclicker.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Autoclicker
+{
+    partial class Autoclicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Autoclicker));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start/Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 13;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Tasto DX",
+            "Tasto SX",
+            "Aggiungi..."});
+            this.comboBox1.Location = new System.Drawing.Point(40, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(107, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Seleziona tasto";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.InterceptArrowKeys = false;
+            this.numericUpDown1.Location = new System.Drawing.Point(153, 36);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(54, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // Autoclicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(249, 141);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Autoclicker";
+            this.Text = "Autoclicker";
+            this.Load += new System.EventHandler(this.Autoclicker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Applicazione/Applicazione/Autoclicker.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+
+namespace Autoclicker
+{
+    public partial class Autoclicker : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        public enum MouseEventFlags
+        {
+            LeftDown = 2,
+            LeftUp = 4
+        }
+
+        public void leftClick(Point p)
+        {
+            mouse_event((int)MouseEventFlags.LeftDown, p.X, p.Y, 0, 0);
+            mouse_event((int)MouseEventFlags.LeftUp, p.X, p.Y, 0, 0);
+        }
+
+        bool esecuzione = false;
+
+        public Autoclicker()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            esecuzione = !esecuzione;
+            timer1.Interval = (int)numericUpDown1.Value;
+            timer1.Enabled = true;
+            if (esecuzione)
+            {
+                timer1.Start();
+                button1.BackColor = Color.GreenYellow;
+            }
+            else
+            {
+                timer1.Stop();
+                button1.BackColor = Color.Transparent;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            leftClick(new Point(MousePosition.X, MousePosition.Y));
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(Keyboard.IsKeyDown(Key.NumPad0))
+            {
+                if (!esecuzione)
+                {
+                    timer1.Interval = (int)numericUpDown1.Value;
+                    timer1.Enabled = true;
+                    esecuzione = true;
+                    timer1.Start();
+                }
+                else      
+                {
+                    timer1.Interval = (int)numericUpDown1.Value;
+                    timer1.Enabled = false;
+                    esecuzione = false;
+                    timer1.Stop();
+                }
+            }
+            //if ((!esecuzione) && Keyboard.IsKeyDown(Key.NumPad0))
+            //{
+            //    timer1.Interval = (int)numericUpDown1.Value;
+            //    timer1.Enabled = true;
+            //    timer1.Start();
+            //}
+            //else if (esecuzione && Keyboard.IsKeyDown(Key.NumPad0))
+            //{
+            //    timer1.Interval = (int)numericUpDown1.Value;
+            //    timer1.Enabled = false;
+            //    timer1.Stop();
+            //}
+        }
+
+        private void Autoclicker_Load(object sender, EventArgs e)
+        {
+            timer2.Enabled = true;
+            timer2.Interval = 1;
+            timer2.Start();
+        }
+    }
+}
